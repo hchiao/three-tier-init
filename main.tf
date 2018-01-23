4f1150dccb5e399a74b1b4eb0be88c63a32af05f
mike.chiao <mike.chiao@stitch.md>
2018-01-23 23:56:22 +1100

    Init

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_s3_bucket" "three-tier-architecture-bucket" {
+    bucket = "three-tier-architecture-bucket"
+    versioning {
+      enabled = true
+    }
+}
+
+resource "aws_dynamodb_table" "three-tier-architecture-table" {
+  name = "three-tier-architecture-table"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags {
+    Name = "three-tier-architecture-table"
+  }
+}
